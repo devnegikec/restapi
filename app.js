545b6712ee9c2157dcedaeedf44d80b97ecb08e5
Devendra Negi <devnegikecjunk@gmail.com>
2016-01-30 22:24:13 +0530

    Basic GET api working

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/fx');
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+  .post(function(req, res){
+    
+  })
+  .get(function(req, res){
+      var query = {};
+      if(req.query.genre){
+        query.genre = req.query.genre;
+      }
+
+      Book.find(query,function(err, books){
+        if(err){
+          res.status(500).send(err);
+        }else{
+          res.json(books);
+        }
+    });
+});
+
+bookRouter.route('/Books/:bookId')
+  .get(function(req,res){
+      Book.findById(req.params.bookId, function(err,book){
+        if(err)
+          res.status(500).send(err);
+        else {
+          res.json(book);
+        }
+    });
+})
+app.use('/api',bookRouter);
+
+app.get('/',function(req, res){
+  res.send('Welcome to my API!');
+});
+
+app.listen(port, function(){
+  console.log('Running on Port uisng gulp:', port);
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+1. install express (npm install express --save)
+2. install Gulp, and setup (npm install gulp --save)
+3. Also install Gulp globaly so that you can run command through your command
+line
+4. install Gulp nodemon (npm install gulp-nodemon --save)
+5. install Mongoose (npm install mongoose --save)
